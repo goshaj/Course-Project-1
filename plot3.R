de58084d57d4f243f62859bc85dfc8893dc8d70f
goshaj <41474559+goshaj@users.noreply.github.com>
2018-08-23 11:05:43 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -3,11 +3,11 @@ dataFile <- "./household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 cutdata <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-datetime<- strptime(paste(subSetData$Date, cutdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-GAP <- as.numeric(subSetData$Global_active_power)
-submetering1 <- as.numeric(subSetData$Sub_metering_1)
-submetering2 <- as.numeric(subSetData$Sub_metering_2)
-submetering3 <- as.numeric(subSetData$Sub_metering_3)
+datetime<- strptime(paste(cutdata$Date, cutdata$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+GAP <- as.numeric(cutdata$Global_active_power)
+submetering1 <- as.numeric(cutdata$Sub_metering_1)
+submetering2 <- as.numeric(cutdata$Sub_metering_2)
+submetering3 <- as.numeric(cutdata$Sub_metering_3)
 png("plot3.png", width=480, height=480)
 plot(datetime, submetering1, type="l", ylab="Energy Submetering", xlab="")
 lines(datetime, submetering2, type="l", col="red")
